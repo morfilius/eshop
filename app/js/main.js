eb5aa493d398c6c61c7320812d70e150032f66a5
Morfilius <morfilius88@gmail.com>
2016-05-12 18:15:25 +0300

    12.04.16 products

--- app/js/main.js
+++ app/js/main.js
@@ -1,26 +1,97 @@
 $(document).ready(function(){
 	
 	///toggle-lang
-
 	var tl = $('.toggle-lang');
 	tl.find('.toggle-lang__menu').hide();
 	tl.on('click',function () {
 		$(this).find('.toggle-lang__menu').toggle();
 	})
 
+
+
 	//search
 	$('.search__x').on('click', function() {
 		$(this).siblings('.search__text').val('');
 	});
+
+
 
 	//carousel-1 //index
 	$('.carousel-1').owlCarousel({
 		items: 1,
 		loop:  true,
 		nav:   true,
-		dot:  true,
+		dot:   true,
 		animateOut: 'fadeOut',
 		navText: ["<i class='fa fa-chevron-left owl__nav-item' aria-hidden='true'></i>","<i class='fa fa-chevron-right owl__nav-item' aria-hidden='true'></i>"],
 	});
+
+
+
+	
+
+
+	products($('.new-products-js'),$('#products__nav-container-js'));
+
+
+
+
+	// функции
+	
+
+	// container    - jq обьект контейнера для итемов овл карусели
+	// navContainer - jq обьект контейнера для кнопок навигации
+
+	function products(container,navContainer) {
+		var npj = container;
+		var next = navContainer.find('.next-js');
+		var prev = navContainer.find('.prev-js');
+
+		npj.owlCarousel({
+			items:      1,
+			animateOut: 'fadeOut',
+			dots:       false,
+		});
+
+		prev.css({
+			opacity: 0.5,
+			cursor: 'default'
+		});
+
+		next.click(function() {
+			npj.trigger('next.owl.carousel');
+		})
+
+		prev.click(function() {
+			npj.trigger('prev.owl.carousel');
+		})
+
+		npj.on('changed.owl.carousel', function(event) {
+			if(event.item.count-1 === event.item.index ) {
+				remove(next,prev);
+				next.css({
+					opacity: 0.5,
+					cursor: 'default'
+				});
+			}else if (event.item.index === 0) {
+				remove(next,prev);
+				prev.css({
+					opacity: 0.5,
+					cursor: 'default'
+				});
+			}else{
+				remove(next,prev);
+			}
+		})
+	}
+
+	//удаление опасити из кнопок навигации
+
+	function remove(next,prev) {
+		next.removeAttr('style');
+		prev.removeAttr('style');
+	}
+
+
 
 })
